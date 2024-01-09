abd62b2ae1b01b93d9487b6cd6cff015dfa4c4f5
nguyenthaominh <nguyenthaominh993@gmail.com>
2024-01-10 00:22:31 +0700

    price

--- routes/stripe.js
+++ routes/stripe.js
@@ -36,7 +36,7 @@ router.post("/create-checkout-session", async (req, res) => {
   const line_items = req.body.cartItems.map((item) => {
     return {
       price_data: {
-        currency: "usd",
+        currency: "vnd",
         product_data: {
           name: item.name,
           imageUrl: item.image,
@@ -45,7 +45,7 @@ router.post("/create-checkout-session", async (req, res) => {
             id: item.id,
           },
         },
-        unit_amount: item.price * 100,
+        unit_amount: item.price,
       },
       quantity: item.cartQuantity,
     };
